c84fbe0ce6090ea5a7a47bedf1aea756025d7b72
Karan <karansethia24@gmail.com>
2022-07-19 12:41:52 +0530

    Creating more child and wrapper componenents

--- app-files/src/components/ExpenseItem.js
+++ app-files/src/components/ExpenseItem.js
@@ -1,13 +1,16 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 
 export default function ExpenseItem(props) {
+
   return (
-    <div className='expense-item'>
-      <div>{props.date.toDateString()}</div>
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date}/>
       <div  className='expense-item__description'>
       <h2>{props.title}</h2>
       <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
