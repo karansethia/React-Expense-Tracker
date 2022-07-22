36d32b14c4aab3223d3fd7051a0deb1352956771
Karan <karansethia24@gmail.com>
2022-07-23 03:22:03 +0530

    Adding State in React

--- app-files/src/components/ExpenseInfo/ExpenseItem.js
+++ app-files/src/components/ExpenseInfo/ExpenseItem.js
@@ -1,18 +1,23 @@
+import { useState } from 'react'
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate';
 import Card from '../UIComp/Card';
 
 export default function ExpenseItem(props) {
 
+const [title, setTitle] = useState(props.title); // using array destructuring to take updated value and assigning function
+
 const clickHandler = () => {
-  console.log("Hello")
+  setTitle('Updated!');
+  console.log("Hello");
+  
 }
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       <div  className='expense-item__description'>
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
       <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick={clickHandler}>Change title</button>
