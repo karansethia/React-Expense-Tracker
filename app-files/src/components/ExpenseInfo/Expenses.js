363fd8db35cda2f2dd6e7aa0465b0c754cc07282
Karan <karansethia24@gmail.com>
2022-07-31 00:24:17 +0530

    Adding Map function to use expense Array dynamically

--- app-files/src/components/ExpenseInfo/Expenses.js
+++ app-files/src/components/ExpenseInfo/Expenses.js
@@ -15,22 +15,22 @@ export default function Expenses(props){
        <div>
         <ExpensesFilter selected={filteredYear} onFilterChange={filterChangeHandler}/>
        <Card className="expenses">
-           <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItem>
+        {props.items.map(expenseparam => (
+          <ExpenseItem title = {expenseparam.title} amount = {expenseparam.amount} date={expenseparam.date} />
+        ))}
        </Card>
        </div>
    ); 
 }
+ /* 
+ ! Hard Coded data down below is not useful so we use Array.map method
+ <ExpenseItem
+ title={props.items[0].title}
+ amount={props.items[0].amount}
+ date={props.items[0].date}
+></ExpenseItem>
+<ExpenseItem
+ title={props.items[1].title}
+ amount={props.items[1].amount}
+ date={props.items[1].date}
+></ExpenseItem> */
