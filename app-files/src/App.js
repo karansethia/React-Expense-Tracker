8847ad845b3f889f3f1c6a9ca8701028d32bbe20
Karan <karansethia24@gmail.com>
2022-08-13 00:59:54 +0530

    Adding State to INITIAL_EXPENSES object in App.js

--- app-files/src/App.js
+++ app-files/src/App.js
@@ -1,33 +1,38 @@
+import { useState } from "react";
 import Expenses from "./components/ExpenseInfo/Expenses";
 import NewExpense from "./components/NewExpenses/NewExpense";
 
-const App = () => {
-const addExpenseHandler = expense => {
-  console.log(expense);
-  console.log("inside app");
-}
 
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+const addExpenseHandler = expense => {
+  setExpenses(prevExpenses => {
+    return [expense,...prevExpenses]
+  });
+};
+
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler}/>
