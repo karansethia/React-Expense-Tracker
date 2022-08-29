570f344505fb32e7d6e1b89e06b91c0323a38b95
Karan <karansethia24@gmail.com>
2022-08-29 22:58:58 +0530

    Editing Chart.js

--- app-files/src/components/Chart/Chart.js
+++ app-files/src/components/Chart/Chart.js
@@ -1,8 +1,17 @@
 import ChartBar from "./ChartBar";
-import './Chart.css';
+import "./Chart.css";
 
-export default function Chart(props){
-    return <div className="chart">
-
+export default function Chart(props) {
+  return (
+    <div className="chart">
+      {props.data.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.id}
+          value={dataPoint.value}
+          maxValue={null}
+          label={dataPoint.label}
+        />
+      ))}
     </div>
+  );
 }
